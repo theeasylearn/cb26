117ac6d7868292db2d2000bb9c5fd55be84cea2a
MahmadHadi <hdnayani@gmail.com>
2024-02-09 08:10:01 +0530

    conditional

--- p1.c
+++
@@ -1,9 +0,0 @@
-
-// Conditional statements: 
-//                         simple if
-//                         if else
-//                         else if ladder
-//                         nested if else
-//                         switch case 
-//                         ternary operators
-
